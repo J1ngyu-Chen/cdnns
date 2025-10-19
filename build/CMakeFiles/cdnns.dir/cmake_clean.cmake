a3cc8adc224d0e2cd7a988bfef43b59afa43ba2d
JingyuChen <jingyuchen.pers@gmail.com>
2025-10-19 18:04:24 +0800

    Remove unnecessary files

--- build/CMakeFiles/cdnns.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcdnns.pdb"
-  "libcdnns.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CUDA)
-  include(CMakeFiles/cdnns.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/cdnns.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cdnns.
-# This may be replaced when dependencies are built.
